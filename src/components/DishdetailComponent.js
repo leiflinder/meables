60fa22c9549b11d52e6dc9c9f5a830b69f508461
leiflinder <leif@leiflinder.com>
2021-12-20 12:06:35 -0500

    2021 12 20 A

---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody,
+    CardTitle } from 'reactstrap';
+
+    class DishDetail extends Compenent{
+        constructor(props){
+            super(props);
+        }
+        render(){
+            return(
+                <div className="container">
+                    
+                </div>
+            );
+        }
+    }
+
+    export default DishDetail;
